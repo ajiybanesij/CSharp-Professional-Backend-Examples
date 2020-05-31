89eb3da9b07b70fc7843fbbe3cedfb63e2bd036b
Ajiyba Nesij <ajiybanesij@hotmail.com>
2020-05-31 15:01:21 +0300

    BAsic IQueryable Repository Added
    ToList yapıldığında context kapanıyor. Contex kapanmadan  IQueryable operasyonlar yapılması için bu yapı oluşturuldu.

---
+++ Backend 1/DevFramework/DevFramework.Core/DataAccess/IQueryableRepository.cs
@@ -0,0 +1,15 @@
+﻿using DevFramework.Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DevFramework.Core.DataAccess
+{
+    //Temel IQueryable Repositorysi
+    //ToList yapıldığında context kapanıyor. Kapanmadan  IQueryable operasyonlar yapılması için bu yapı oluşturuldu.
+    public interface IQueryableRepository<T> where T:class,IEntity,new()
+    {
+        IQueryable<T> Table { get; }
+    }
+}
