fa49457d77f6d6d2a55570039e2bb76d5d00096e
Ajiyba Nesij <ajiybanesij@hotmail.com>
2020-05-31 18:51:43 +0300

    Entity Framework Implementasyonunun Gerçekleştirilmesi

---
+++ Backend 1/DevFramework/DevFramework.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using DevFramework.Core.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFramework.Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase <TEntity,TContex> : IEntityRepository<TEntity>:
+        where TEntity:class,IEntity,new()
+        where TContex:DbContext,new()
+    {
+    }
+}
