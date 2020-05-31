2bf7fedfdfa839d17c337a9a96a599a5bba5ba34
Ajiyba Nesij <ajiybanesij@hotmail.com>
2020-05-31 19:04:47 +0300

    Entity Framework Implementasyonu Gerçekleştirilmesi 2

--- Backend 1/DevFramework/DevFramework.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Backend 1/DevFramework/DevFramework.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -2,13 +2,64 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DevFramework.Core.DataAccess.EntityFramework
 {
-    public class EfEntityRepositoryBase <TEntity,TContex> : IEntityRepository<TEntity>:
-        where TEntity:class,IEntity,new()
-        where TContex:DbContext,new()
+    public class EfEntityRepositoryBase<TEntity, TContex> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContex : DbContext, new()
     {
+        public TEntity Add(TEntity entity)
+        {
+            using (var context=new TContex())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+                return entity;
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var context = new TContex())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (var context=new TContex())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (var context=new TContex())
+            {
+                return filter == null ?
+                    context.Set<TEntity>().ToList() :
+                    context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            using (var context = new TContex())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+                return entity;
+            }
+        }
     }
 }
